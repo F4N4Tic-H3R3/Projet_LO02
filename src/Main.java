6ec4b80e3b2ec23bfc6266f60c12b9e1cca7b1e0
F4N4Tic-H3R3 <nicolas.sarrouy@utt.fr>
2024-11-29 19:52:57 +0100

    test pour enlever une branche

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
         System.out.println("test branche Git");
 
+
+        System.out.println("Test d'enlèvement de branche");
     }
     // Well, I truly hope that it will go uphill from here on out...
     //Here goes nothing !
