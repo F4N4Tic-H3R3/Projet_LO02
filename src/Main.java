49e8013c4c6fd8b0d9a06818c141f39f88ca150c
F4N4Tic-H3R3 <nicolas.sarrouy@utt.fr>
2024-11-29 18:53:07 +0100

    Test Commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    // Well, I truly hope that it will go uphill from here on out...
 }
